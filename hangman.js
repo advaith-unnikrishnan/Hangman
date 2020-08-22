dcb27f35f0c003ffecf49907d504bc019d25637b
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2020-08-22 10:44:00 +0530

    Update

--- hangman.js
+++ hangman.js
@@ -54,3 +54,19 @@ const displayWord=(e)=>{
         updateImg()
     }
 }
+
+function generateButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+      `
+        <button
+          class="btn btn-lg btn-primary m-2"
+          id='` + letter + `'
+          onClick="handleGuess('` + letter + `')"
+        >
+          ` + letter + `
+        </button>
+      `).join('');
+  
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
+  }
+  
