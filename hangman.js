2179402d03612e080666a2d6e888fd153d7e40ae
ADVAITH U <45172876+advaith-unnikrishnan@users.noreply.github.com>
2020-08-23 12:43:15 +0530

    Add functions to check game status, update img , handle the guesses

--- hangman.js
+++ hangman.js
@@ -1,3 +1,4 @@
+import swal from 'sweetalert'
 let characters=[
   "IRONMAN",
   "CAPTAIN AMERICA",
@@ -14,25 +15,17 @@ let characters=[
   "DAREDEVIL",
   "STAN LEE"
 ]
-
-
-const updateImg=()=>{
-    ++img_name
-    if(moves>0)
-    {
-        img.src="images/hangman"+img_name+".png"
-        moves--
-    }
-    else if(moves==0)
-        alert("Game Over!!")
-}
-
+let answer=''
+let guessed=[]
+let moves=6
+let mistakes=0
+let wordStatus=null
 /*function to pick a random word from the list*/
 function randomWord() {
   answer = characters[Math.floor(Math.random() * characters.length)];
 }
 
-
+/*function to generate buttons*/
 function generateButtons() {
     let buttonsHTML = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter =>
       `
@@ -47,5 +40,36 @@ function generateButtons() {
     document.getElementById('keyboard').innerHTML = buttonsHTML;
 }
 
+/*function to handle the guess made by user*/
+function handleGuess(chosen) {
+  guessed.indexOf(chosen)===-1?guessed.push(chosen):null
+  document.getElementById(chosen).setAttribute('disable',true)
+  if (answer.indexOf(chosen)>=0)
+  {
+    guessedWord()
+    checkGameStatus()
+  } 
+  else if(answer.indexOf(chosen)===-1)
+  {
+    updateMistakes()
+    checkGameStatus()
+    updateImg()
+  }  
+}
 
+/* Function to check whether game won or lost*/
+function checkGameStatus() {
+  if(wordStatus === answer){
+    swal("You won!!")
+  }
+  else if(mistakes===moves){
+    swal("You lost!","The answer was "+answer)
+  }
+}
+
+// Function to update hangman image
+function updateImg() {
+  document.getElementById('hangman-img').src="./images/hangman"+mistakes+".png"  
+}
+randomWord();
 generateButtons();
